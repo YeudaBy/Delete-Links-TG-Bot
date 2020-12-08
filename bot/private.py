8c37f680177ed71caa2b7c63b0edaf263ddb4281
M100achuz2 <68661509+M100achuz2@users.noreply.github.com>
2020-12-09 00:25:08 +0200

    Update private.py
    ערוך את username הבוט בשורה 9

--- bot/private.py
+++ bot/private.py
@@ -5,7 +5,8 @@ path_users = 'bot/json_bot/users.json'
 path_msg_text = 'bot/json_bot/MSG.json'
 def private_start(c, m):
     m.reply(json_load(path_msg_text)['start_message'].format(
-        f'[{m.from_user.first_name}](tg://user?id={m.from_user.id})'),
+        f'[{m.from_user.first_name}](tg://user?id={m.from_user.id})',
+    'DeleteTGLinkesHEbot'),
         disable_web_page_preview=True,
         reply_markup=kmark([[kbut("עדכוני רובוטים 👽", url= "t.me/m100achuzBots")]]))
     users = json_load(path_users)
